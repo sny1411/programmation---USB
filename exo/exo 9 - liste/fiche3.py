3e80def848e9a8f32d3067f8a535b206c7356a66
sny1411 <matteo2004@outlook.fr>
2020-11-09 08:53:50 +0100

    fiche 3, exo 2,3,4,5

--- exo/exo 9 - liste/fiche3.py
+++ exo/exo 9 - liste/fiche3.py
@@ -4,8 +4,9 @@
 villes = ["Bordeaux","Paris"]
 towns = villes.copy()
 towns.append("Lille")
-print(villes)
-print(towns)
+#print(villes)
+#print(towns)
+
 
 #exercice 2:
 
@@ -24,12 +25,108 @@ def pairs_impairs(liste):
 
 listeInt = [10,2,3,7,16]
 
-print(pairs_impairs(listeInt))
+#print(pairs_impairs(listeInt))
 
 #exercice 3:
 
 # 1)
 
 def multiples1(n):
-    for i in range(10):
+    """renvoie la liste des 10 premier multiple de n
+    entrée: n de type int
+    sortie: list"""
+    listeM = []
+    if n < 0:
+        pass
+    else:
+        for i in range(1,11):
+            listeM.append(n*i)
+        return listeM
+
+#print(multiples1(5))
+
+def multiples2(n):
+    """renvoie la liste des multiple de n strictement inférieur à 1000
+    entrée: n de type int
+    sortie: list"""
+    listeM = []
+    if n < 0:
+        pass
+    else:
+        i = 1
+        while i * n < 1000:
+            listeM.append(i * n)
+            i += 1
+        print(len(listeM))
+        return listeM
+
+
+#print(multiples2(6))
+
+
+def diviseur(n):
+    """renvoie la liste des diviseur de n
+    entrée: n de type int
+    sortie: list"""
+    listeD = []
+    if n < 0:
+        pass
+    else:
+        for i in range(1,100):
+            if n % i == 0:
+                listeD.append(i)
+        return listeD
+#print(diviseur(70))
+
+#exo 4:
+
+#1:
+
+
+def listConstruc1(n):
+    listCreated = []
+    i = 1
+    while i - 1< n:
+        listCreated.extend([1,i])
+        i += 1
+    return listCreated
+
+#print(listConstruc1(10))
+        
+#2:
+
+def listConstruc2(n):
+    listCreated = []
+    x = 0
+    while x != n:
+        x += 1
+        for i in range(x):
+            listCreated.append(i + 1)
+        
+    return listCreated
+
+#print(listConstruc2(10))
+
+#exercice 5:
+
+def mystere(liste1,liste2):
+    """ liste 1 et liste2 sont deux listes de nbre entiers rangés dans l'ordre croissant"""
+    liste = []
+    i,j = 0,0
+    while i < len(liste1) and j < len(liste2):
+        if liste1[i] < liste2[j]:
+            liste.append(liste1[i])
+            i += 1
+        else:
+            liste.append(liste2[j])
+            j += 1
+    return liste
+
+print(mystere([2,5,6,8],[1,4,7,8,9]))
+
+#exercice 6:
+
+def nomPrenom():
+    nom_prenom = input("entrez")
+
         
